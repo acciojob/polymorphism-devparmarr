c25bd953b127b36a51249115143fd42a387206d2
devparmarr <devparmarr123@gmail.com>
2023-04-15 03:40:48 +0530

    assignmet

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    Product p= new Product();
+    int result1 = p.product(5, 10);
+    int result2 = p.product(5, 10, 15);
+    double result3 = p.product(5, 10);
 
+}
+
+class Product{
+    public int product(int x, int y) {
+
+        return x+y;
+    }
+
+    public int product(int x, int y, int z) {
+
+        return x+y+z;
+    }
+
+    public double product(double x, double y) {
+
+        return x+y;
+    }
 }
